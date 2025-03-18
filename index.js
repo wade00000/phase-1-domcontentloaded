916814d4d81d65d61401bd14c8cf5c40e278f2b2
WADE <MEANTOINE12@GMAIL.COM>
2025-03-18 21:03:46 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",changeText)
+       
+function changeText(){
+       let words = document.getElementById('text')
+       words.textContent = "This is really cool!"
+        
+}
+
+
+
+
+
+
+
+    
+
+
